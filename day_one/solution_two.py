492792d24db05261480b36fb686b36e8f41a6ae8
Chris Russell-Walker <christopher.russell-walker@datarobot.com>
2021-12-03 16:37:29 -0500

    Solved second part of day one

---
+++ day_one/solution_two.py
@@ -0,0 +1,26 @@
+from utils.get_input import get_for_day
+
+
+def solve_puzzle():
+    increased_count = 0
+    # TODO - Move this to a reusable utility function - the parsing of the lines
+    entries = [int(val.strip()) for val in get_for_day(1).split('\n') if val.strip()]
+
+    cumulative_depths = []
+    # Create a list of values:
+    for idx in range(len(entries)):
+        cumulative_depths.append(
+            sum(entries[idx:idx+3])
+        )
+
+    prev_depth = cumulative_depths.pop(0)
+    for entry in cumulative_depths:
+        if entry > prev_depth:
+            increased_count += 1
+        prev_depth = entry
+
+    print(f'{increased_count=}')
+
+
+if __name__ == '__main__':
+    solve_puzzle()
