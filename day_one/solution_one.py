9b90a925635e705d5ca664d349ac795fd3679446
Chris Russell-Walker <christopher.russell-walker@datarobot.com>
2021-12-03 16:28:56 -0500

    Clean up code

--- day_one/solution_one.py
+++ day_one/solution_one.py
@@ -4,19 +4,13 @@ from utils.get_input import get_for_day
 def solve_puzzle():
     increased_count = 0
     # TODO - Move this to a reusable utility function - the parsing of the lines
-    entries = get_for_day(1).split('\n')
-    prev_depth = int(entries.pop(0))
-    for entry in entries:
-        if not entry:
-            pass
-        try:
-            if int(entry) > prev_depth:
-                increased_count += 1
-        except ValueError:
-            print(f"Entry not an integer: {entry}")
-            continue
+    entries = [int(val.strip()) for val in get_for_day(1).split('\n') if val.strip()]
+    prev_depth = entries.pop(0)
 
-        prev_depth = int(entry)
+    for entry in entries:
+        if entry > prev_depth:
+            increased_count += 1
+        prev_depth = entry
 
     print(f'{increased_count=}')
 
